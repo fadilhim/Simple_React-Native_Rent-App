f70d735ea94d241ad4e0cc216bf70840cb726db6
Poko Groot <PokoGroot@users.noreply.github.com>
2019-09-06 15:00:11 +0700

    redux integration in book detail

--- src/redux/actions/book.js
+++ src/redux/actions/book.js
@@ -1,30 +1,30 @@
 /* eslint-disable prettier/prettier */
 import Axios from 'axios'
 
-// const uerel = 'https://rent-book-libs.herokuapp.com'
-const uerel = 'http://192.168.6.113:3030'
+// const url = 'https://rent-book-libs.herokuapp.com'
+const url = 'http://192.168.6.113:3030'
 // const token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwiZW1haWwiOiJmYWRpbGhpbWF3YW5AZ21haWwuY29tIiwibGV2ZWwiOiJhZG1pbiIsInVzZXJuYW1lIjoiZmFkaWwiLCJmdWxsbmFtZSI6ImZhZGlsIiwiaWF0IjoxNTY3Njg5MzA3fQ.pjbG7vTxDxXd6AU_PXp7MVE3r1y48Y-31xB2k7da_1U"
 
 export const getBook = (address, search) => {
-    let url = address ? `${address}` : `${uerel}/book/?sortby=book_id`
-    if (search !== undefined) url += `&search=${search}`
+    // let alamat = address ? `${address}` : `${url}/book/?sortby=book_id`
+    // if (search !== undefined) alamat += `&search=${search}`
     return {
         type: 'GET_BOOKS',
-        payload: Axios.get(`${url}`)
+        payload: Axios.get(`${url}/book/?sortby=book_id`)
     }
 }
 
 export const getBookById = (id) => {
     return {
         type: 'GET_BOOK_BY_ID',
-        payload: Axios.get(`${uerel}/book/${id}`)
+        payload: Axios.get(`${url}/book/${id}`)
     }
 }
 
 export const donateBook = (data) => {
     return {
         type: 'DONATE_BOOK',
-        payload: Axios.post(`${uerel}/book/`,
+        payload: Axios.post(`${url}/book/`,
                 data,
                 { headers: { Authorization: token}})
     }
